6f5be1c2dc4946ee35515ac4ea21b2173afb2193
Doug Moreira <dougmoreira@dougs-mbp.home>
2021-09-24 17:41:43 -0400

    bug bus

--- controllers/session_controllers.js
+++ controllers/session_controllers.js
@@ -13,7 +13,7 @@ sessions.post('/',(req,res)=>{
             console.log(err)
             res.send('oops the db had a problem!')
         }else if(!foundUser){
-            res.sendStatus(status)
+            res.send("<a href="/">Sorry, no username found</a>")
         }else{
             if(bcrypt.compareSync(req.body.password, foundUser.password)){
                 req.session.currentUser = foundUser
